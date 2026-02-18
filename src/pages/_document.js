f068ac47a43d44df721d90ab10711811a63f41df
Ariel Jericko Gacilo <bokzgacilo.com>
2026-02-19 02:49:51 +0800

    feat: add Google AdSense script to `_document.js`

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5701102950204692"
+          crossOrigin="anonymous"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
